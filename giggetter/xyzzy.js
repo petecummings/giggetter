1af5691999d0d958b875f6a92be78c3557f0cf24
pete <pete@concord-eastech-net>
2017-11-25 21:28:03 -0500

    why
    Change-Id: I6deddc73b913c826efb97b7e90abe5d4ae79e24a
    Signed-off-by: pete <pete@concord-eastech-net>

--- giggetter/xyzzy.js
+++ giggetter/xyzzy.js
@@ -3,6 +3,27 @@
  */
 
 
-
+var aaa=0;
 
 var tt;
+
+
+
+//# giggetter
+//
+//
+//
+//## Usage
+//
+//
+//
+//## Developing
+//
+
+
+### Tools
+
+Created with [Nodeclipse](https://github.com/Nodeclipse/nodeclipse-1)
+ ([Eclipse Marketplace](http://marketplace.eclipse.org/content/nodeclipse), [site](http://www.nodeclipse.org))   
+
+Nodeclipse is free open-source project that grows with your contributions.
